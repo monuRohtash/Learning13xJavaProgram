5198e1fe0c5b980c42a6537218797c2d26e3e5be
Raghav <rohtashkumar.mk87@gmail.com>
2025-06-11 17:39:39 +0530

    puch to third file

--- src/ex_01_java_Basic/Lab002_push_to_git.java
+++ src/ex_01_java_Basic/Lab002_push_to_git.java
@@ -4,5 +4,9 @@ public class Lab002_push_to_git {
     public static void main(String [] args){
 
         System.out.println("puch to code i github !");
+
+        System.out.println("puch to code i github !");
+
+
     }
 }
