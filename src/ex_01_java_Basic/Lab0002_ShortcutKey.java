728ab6d8818916cd5582d45a12a7ee1ed96610c3
Raghav <rohtashkumar.mk87@gmail.com>
2025-06-15 19:34:10 +0530

    push

--- src/ex_01_java_Basic/Lab0002_ShortcutKey.java
+++ src/ex_01_java_Basic/Lab0002_ShortcutKey.java
@@ -1,6 +1,6 @@
 package ex_01_java_Basic;
 
-public class Lab002_ShortcutKey {
+public class Lab0002_ShortcutKey {
     public static void main(String[] args) {
         System.out.println("Shourt cut key for main method -> typ.. main");
         System.out.println("shourt cut key for print ant statement -> typ.. sout");
